571c1a3da018429cdf4da1a9c85475680ff93620
sosskanyita <soss.mwangi97@gmail.com>
2019-11-06 11:34:39 +0300

    modify index and javascript

---
+++ JS/script.js
@@ -0,0 +1,15 @@
+
+$(document).ready(function(){
+    $(".img1").click(function(){
+        $(".info1").show();
+        $("img1").hide();
+});
+$(".img1").click(function(){
+    $(".img1").hide();
+});
+$(".info1").click(function(){
+    $(".img1").show();
+});
+$(".info1").click(function(){
+    $(".info1").hide();
+});
